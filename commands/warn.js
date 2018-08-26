1ca36212deef667aa2bf0f0116c48035a2e81f58
TheOnlyWalrusYT <38198747+TheOnlyWalrusYT@users.noreply.github.com>
2018-08-26 03:47:24 -0400

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -17,9 +17,43 @@ module.exports.run = async (bot, message, args) => {
 
     warns[wUser.id].warns++;
 
-    fs.writeFile("./warnings.json", JSON.stringify(warns) (err) => {
-        if (err) {console.log(err);
-    })
+    fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+        if (err) console.log(err);
+    });
+
+    let warnEmbed = new Discord.RichEmbed()
+    .setDescription("Warns")
+    .setAuthor(message.author.username)
+    .setColor("#ffff00")
+    .addField("Warned User", wUser.tag)
+    .addField("Channel Warned In", message.channel)
+    .addField("Number of Warnings", warns[wUser.id].warns)
+    .addField("Reason", reason);
+
+    let warnchannel = message.guild.channels.find(`name`, "warnings");
+    if(!warnchannel) return message.channel.send("Couldn't find a warnings channel. If there is one make sure it is spelled correctly.");
+
+    warnchannel.send(warnEmbed);
+
+    if(warns[wUser.id].warns == 2){
+        let muterole = message.guild.roles.find(`name`, "muted");
+        if(!muterole) return message.channel.send("There is no muted role!");
+
+        let mutetime = "1m";
+        await(wUser.addRole(muterole.id));
+        message.channel.send(`${wUser.tag} has been muted for 1 minute for recieving their second warning!`);
+
+        setTimeout(function(){
+            wUser.removeRole(muterole.id)
+            message.channel.send(`${wUser.tag} has been unmuted!`)
+        })
+    }
+    
+    if(warns[wUser.id].warns == 5){
+        message.guild.member(wUser).ban(reason);
+        message.channel.send(`${wUser.tag} has been banned for recieving their fifth warning!`);
+    }
+
 
 }
 
